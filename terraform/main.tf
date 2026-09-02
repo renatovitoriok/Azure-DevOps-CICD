e16d1aaba693a549de7cac2ae4a4fd959406e755
renatovitoriok <renatovitorio@gmail.com>
2026-09-02 10:58:58 -0300

    infra: configura remote state do Terraform (#26)
    * infra: configura remote state do Terraform
    
    * infra: configura remote state e stage Terraform
    
    ---------
    
    Co-authored-by: Renato Vitorio <renato_vitorio@yahoo.com.br>

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-cicd-lab"
+    storage_account_name = "stcicdlabtfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -14,4 +21,18 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "rg-cicd-lab"
   location = "Brazil South"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "stcicdlabtfstate"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
 }
